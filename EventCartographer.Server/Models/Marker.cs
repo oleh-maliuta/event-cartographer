3e53c69162666b3b00b12257c786f6f37d8c3960
OlehMaliuta <OlehMaliutaGitHub@gmail.com>
2024-04-12 22:19:34 +0300

    added models for the database

---
+++ EventCartographer.Server/Models/Marker.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EventCartographer.Server.Models
+{
+    public class Marker
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("userId")]
+        public Guid UserId { get; set; }
+
+        [BsonElement("title")]
+        public string Title { get; set; }
+        [BsonElement("description")]
+        public string Description { get; set; }
+        [BsonElement("importance")]
+        public string Importance { get; set; }
+        [BsonElement("startsAt")]
+        public DateTime StartsAt { get; set; }
+    }
+}
---
+++ EventCartographer.Server/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace EventCartographer.Server.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("password")]
+        public string Password { get; set; }
+    }
+}
