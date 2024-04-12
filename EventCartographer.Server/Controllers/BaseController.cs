77f8f6615748742242c0d9b123213264ef8e4f87
OlehMaliuta <OlehMaliutaGitHub@gmail.com>
2024-04-13 00:31:38 +0300

    added base controller and some http response bodies

---
+++ EventCartographer.Server/Controllers/BaseController.cs
@@ -0,0 +1,39 @@
+﻿using EventCartographer.Server.Models;
+using EventCartographer.Server.Services;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using System.Security.Claims;
+
+namespace EventCartographer.Server.Controllers
+{
+    public abstract class BaseController : ControllerBase
+    {
+        protected MongoDbService DB { get; }
+        protected string AuthorizedUserId
+        {
+            get
+            {
+                string? id = HttpContext.User.Claims
+                    .FirstOrDefault(p => p.Type == ClaimTypes.NameIdentifier)?.Value;
+
+                return id ?? throw new InvalidOperationException(
+                    "This property accessible only for authorized users.");
+            }
+        }
+        protected User AuthorizedUser
+        {
+            get
+            {
+                User? user = DB.Users.Find(p => p.Id == AuthorizedUserId).FirstOrDefault();
+
+                return user ?? throw new InvalidOperationException(
+                    "This property accessible only for authorized users.");
+            }
+        }
+
+        public BaseController(MongoDbService db)
+        {
+            DB = db;
+        }
+    }
+}
---
+++ EventCartographer.Server/Responses/UserResponse.cs
@@ -0,0 +1,29 @@
+﻿using EventCartographer.Server.Models;
+using System.Text.Json.Serialization;
+
+namespace EventCartographer.Server.Responses
+{
+    public class UserResponse : BaseResponse
+    {
+        public UserResponse(User data) : base(true, null, null)
+        {
+            Data = new View(data);
+        }
+
+        public UserResponse(ICollection<User> user) : base(true, null, null)
+        {
+            Data = user.Select(p => new View(p));
+        }
+
+        public class View
+        {
+            [JsonPropertyName("name")]
+            public string Name { get; set; }
+
+            public View(User user)
+            {
+                Name = user.Name;
+            }
+        }
+    }
+}
