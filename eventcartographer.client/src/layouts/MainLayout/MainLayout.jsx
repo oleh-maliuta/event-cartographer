87365ea5791c02507ce1d9bf951ffc07b23a1fbe
Oleh Maliuta <104268973+OlehMaliuta@users.noreply.github.com>
2024-07-28 22:41:46 +0300

    icon and code improvements
    client:
    * changed client app icon;
    * css code improvements in the css files for the layouts;
    * moved some functions from one place to another in the code of the map page;
    * added the loading animation for the button for a new marker adding and the button for a marker editing.

--- eventcartographer.client/src/layouts/MainLayout/MainLayout.jsx
+++ eventcartographer.client/src/layouts/MainLayout/MainLayout.jsx
@@ -22,6 +22,7 @@ export default function MainLayout() {
     const [isMarkerListFilterVisible, setMarkerListFilterVisibility] = React.useState(false);
 
     const [markersForListLoading, setMarkersForListLoading] = React.useState(false);
+    const [updatingMarkerList, setUpdatingMarkerList] = React.useState(false);
 
     const [markerListSearch, setMarkerListSearch] = React.useState('');
     const [markerListSort, setMarkerListSort] = React.useState({ type: 'importance', asc: true });
@@ -76,34 +77,6 @@ export default function MainLayout() {
         return processedDateTime.toISOString().slice(0, 19);
     }
 
-    async function logOutRequest() {
-        const response = await fetch(`${HOST}:${API_PORT}/api/users/logout`, {
-            method: "GET",
-            mode: "cors",
-            credentials: "include"
-        });
-        const json = await response.json();
-
-        if (response.ok) {
-            window.location.href = `${HOST}:${CLIENT_PORT}/sign-in`;
-        } else if (!response.ok) {
-            if (json.message) {
-                alert(json.message);
-            } else {
-                let errors = "";
-                for (const prop in json.errors) {
-                    for (const err in json.errors[prop]) {
-                        errors += `${json.errors[prop][err]}\n`;
-                    }
-                }
-                errors = errors.slice(0, -1);
-                alert(errors);
-            }
-        } else if (response.status >= 500 && response.status <= 599) {
-            alert("Server error.");
-        }
-    }
-
     async function loadUserInfo() {
         const response = await fetch(`${HOST}:${API_PORT}/api/users/self`, {
             method: "GET",
@@ -150,6 +123,141 @@ export default function MainLayout() {
 
         setMarkersForList(json.data || []);
         setMarkersForListLoading(false);
+    }
+
+    async function logOutRequest() {
+        const response = await fetch(`${HOST}:${API_PORT}/api/users/logout`, {
+            method: "GET",
+            mode: "cors",
+            credentials: "include"
+        });
+        const json = await response.json();
+
+        if (response.ok) {
+            window.location.href = `${HOST}:${CLIENT_PORT}/sign-in`;
+        } else if (!response.ok) {
+            if (json.message) {
+                alert(json.message);
+            } else {
+                let errors = "";
+                for (const prop in json.errors) {
+                    for (const err in json.errors[prop]) {
+                        errors += `${json.errors[prop][err]}\n`;
+                    }
+                }
+                errors = errors.slice(0, -1);
+                alert(errors);
+            }
+        } else if (response.status >= 500 && response.status <= 599) {
+            alert("Server error.");
+        }
+    }
+
+    async function addMarkerRequest() {
+        setUpdatingMarkerList(true);
+
+        const response = await fetch(`${HOST}:${API_PORT}/api/markers`, {
+            method: "POST",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                latitude: Number(latitudeInputRef.current.value) || null,
+                longitude: Number(longitudeInputRef.current.value) || null,
+                startsAt: new Date(startsAtInputRef.current.value) || null,
+                importance: importanceInputRef.current.value || null,
+                title: titleInputRef.current.value || null,
+                description: descriptionInputRef.current.value || null
+            })
+        });
+        const json = await response.json();
+
+        if (response.ok) {
+            const newMapMarker = {
+                id: json.data.id,
+                importance: json.data.importance,
+                latitude: json.data.latitude,
+                longitude: json.data.longitude
+            };
+
+            setMarkersForMap([...markersForMap, newMapMarker]);
+            setMarkersForList([...markersForList, json.data]);
+            setMarkerMenu('list');
+            setNewMarker(null);
+        } else if (!response.ok) {
+            if (json.message) {
+                alert(json.message);
+            } else {
+                let errors = "";
+                for (const prop in json.errors) {
+                    for (const err in json.errors[prop]) {
+                        errors += `${json.errors[prop][err]}\n`;
+                    }
+                }
+                errors = errors.slice(0, -1);
+                alert(errors);
+            }
+        } else if (response.status >= 500 && response.status <= 599) {
+            alert("Server error.");
+        }
+
+        setUpdatingMarkerList(false);
+    }
+
+    async function editMarkerRequest() {
+        setUpdatingMarkerList(true);
+
+        const response = await fetch(`${HOST}:${API_PORT}/api/markers/${editingMarker.id}`, {
+            method: "PUT",
+            mode: "cors",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                latitude: Number(latitudeInputRef.current.value) || null,
+                longitude: Number(longitudeInputRef.current.value) || null,
+                startsAt: new Date(startsAtInputRef.current.value) || null,
+                importance: importanceInputRef.current.value || null,
+                title: titleInputRef.current.value || null,
+                description: descriptionInputRef.current.value || null
+            })
+        });
+        const json = await response.json();
+
+        if (response.ok) {
+            const newList = markersForList.map((el) => {
+                if (el.id === json.data.id) {
+                    return json.data;
+                }
+
+                return el;
+            });
+
+            setMarkersForMap(newList);
+            setMarkersForList(newList);
+            setMarkerMenu('list');
+            setEditingMarker(null);
+        } else if (!response.ok) {
+            if (json.message) {
+                alert(json.message);
+            } else {
+                let errors = "";
+                for (const prop in json.errors) {
+                    for (const err in json.errors[prop]) {
+                        errors += `${json.errors[prop][err]}\n`;
+                    }
+                }
+                errors = errors.slice(0, -1);
+                alert(errors);
+            }
+        } else if (response.status >= 500 && response.status <= 599) {
+            alert("Server error.");
+        }
+
+        setUpdatingMarkerList(false);
     }
 
     function renderMarkersOnMap() {
@@ -570,53 +678,22 @@ export default function MainLayout() {
                                     setMarkerMenu('list');
                                 }}>Cancel</button>
                             <button className={`${cl.editing_marker_button} ${cl.editing_marker_add_button}`}
-                                onClick={async () => {
-                                    const response = await fetch(`${HOST}:${API_PORT}/api/markers`, {
-                                        method: "POST",
-                                        mode: "cors",
-                                        credentials: "include",
-                                        headers: {
-                                            "Content-Type": "application/json"
-                                        },
-                                        body: JSON.stringify({
-                                            latitude: Number(latitudeInputRef.current.value) || null,
-                                            longitude: Number(longitudeInputRef.current.value) || null,
-                                            startsAt: new Date(startsAtInputRef.current.value) || null,
-                                            importance: importanceInputRef.current.value || null,
-                                            title: titleInputRef.current.value || null,
-                                            description: descriptionInputRef.current.value || null
-                                        })
-                                    });
-                                    const json = await response.json();
-
-                                    if (response.ok) {
-                                        const newMapMarker = {
-                                            id: json.data.id,
-                                            importance: json.data.importance,
-                                            latitude: json.data.latitude,
-                                            longitude: json.data.longitude
-                                        };
-
-                                        setMarkersForMap([...markersForMap, newMapMarker]);
-                                        setMarkersForList([...markersForList, json.data]);
-                                        setMarkerMenu('list');
-                                    } else if (!response.ok) {
-                                        if (json.message) {
-                                            alert(json.message);
-                                        } else {
-                                            let errors = "";
-                                            for (const prop in json.errors) {
-                                                for (const err in json.errors[prop]) {
-                                                    errors += `${json.errors[prop][err]}\n`;
-                                                }
-                                            }
-                                            errors = errors.slice(0, -1);
-                                            alert(errors);
-                                        }
-                                    } else if (response.status >= 500 && response.status <= 599) {
-                                        alert("Server error.");
+                                onClick={() => {
+                                    if (!updatingMarkerList) {
+                                        addMarkerRequest();
                                     }
-                                }}>Add</button>
+                                }}>
+                                {
+                                    updatingMarkerList ?
+                                        <LoadingAnimation
+                                            curveColor1="#FFFFFF"
+                                            curveColor2="#00000000"
+                                            size="15px"
+                                            curveWidth="3px" />
+                                        :
+                                        <span>Add</span>
+                                }
+                            </button>
                         </div>
                         :
                         <div className={`${cl.editing_marker_buttons}`}>
@@ -626,54 +703,22 @@ export default function MainLayout() {
                                     setMarkerMenu('list');
                                 }}>Go back to list</button>
                             <button className={`${cl.editing_marker_button} ${cl.editing_marker_edit_button}`}
-                                onClick={async () => {
-                                    const response = await fetch(`${HOST}:${API_PORT}/api/markers/${editingMarker.id}`, {
-                                        method: "PUT",
-                                        mode: "cors",
-                                        credentials: "include",
-                                        headers: {
-                                            "Content-Type": "application/json"
-                                        },
-                                        body: JSON.stringify({
-                                            latitude: Number(latitudeInputRef.current.value) || null,
-                                            longitude: Number(longitudeInputRef.current.value) || null,
-                                            startsAt: new Date(startsAtInputRef.current.value) || null,
-                                            importance: importanceInputRef.current.value || null,
-                                            title: titleInputRef.current.value || null,
-                                            description: descriptionInputRef.current.value || null
-                                        })
-                                    });
-                                    const json = await response.json();
-
-                                    if (response.ok) {
-                                        const newList = markersForList.map((el) => {
-                                            if (el.id === json.data.id) {
-                                                return json.data;
-                                            }
-
-                                            return el;
-                                        });
-
-                                        setMarkersForMap(newList);
-                                        setMarkersForList(newList);
-                                        setMarkerMenu('list');
-                                    } else if (!response.ok) {
-                                        if (json.message) {
-                                            alert(json.message);
-                                        } else {
-                                            let errors = "";
-                                            for (const prop in json.errors) {
-                                                for (const err in json.errors[prop]) {
-                                                    errors += `${json.errors[prop][err]}\n`;
-                                                }
-                                            }
-                                            errors = errors.slice(0, -1);
-                                            alert(errors);
-                                        }
-                                    } else if (response.status >= 500 && response.status <= 599) {
-                                        alert("Server error.");
+                                onClick={() => {
+                                    if (!updatingMarkerList) {
+                                        editMarkerRequest();
                                     }
-                                }}>Edit</button>
+                                }}>
+                                {
+                                    updatingMarkerList ?
+                                        <LoadingAnimation
+                                            curveColor1="#FFFFFF"
+                                            curveColor2="#00000000"
+                                            size="15px"
+                                            curveWidth="3px" />
+                                        :
+                                        <span>Edit</span>
+                                }
+                            </button>
                         </div>
                 }
             </>
